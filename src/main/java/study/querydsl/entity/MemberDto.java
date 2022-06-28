f606e9edc6e1865db55dd8385b425ecad1916976
김진성 <kimjinseong@gimjinseong-ui-MacBookPro.local>
2022-06-28 18:08:39 +0900

    querydsl basic join

---
+++ src/main/java/study/querydsl/entity/MemberDto.java
@@ -0,0 +1,16 @@
+package study.querydsl.entity;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class MemberDto {
+
+    private String username;
+    private int age;
+
+
+}
--- src/test/java/study/querydsl/entity/MemberTest.java
+++ src/test/java/study/querydsl/entity/MemberTest.java
@@ -1,20 +1,36 @@
 package study.querydsl.entity;
 
+import com.querydsl.core.Tuple;
+import com.querydsl.core.types.Projections;
+import com.querydsl.jpa.JPAExpressions;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
 import javax.persistence.PersistenceContext;
+import javax.persistence.PersistenceUnit;
 import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+import static study.querydsl.entity.QMember.member;
+import static study.querydsl.entity.QTeam.team;
 
 @SpringBootTest
 @Transactional
 public class MemberTest {
     @PersistenceContext
     EntityManager em;
-    @Test
-    public void testEntity() {
+
+    JPAQueryFactory queryFactory;
+
+    @BeforeEach
+    public void before() {
+        queryFactory = new JPAQueryFactory(em);
         Team teamA = new Team("teamA");
         Team teamB = new Team("teamB");
         em.persist(teamA);
@@ -27,12 +43,167 @@ public class MemberTest {
         em.persist(member2);
         em.persist(member3);
         em.persist(member4);
+    }
 
-        List<Member> members = em.createQuery("select m from Member m",
-                        Member.class)
-                .getResultList();
-        for (Member member : members) {
-            System.out.println("member=" + member);
-            System.out.println("-> member.team=" + member.getTeam());
-        } }
+    @Test
+    public void startQuerydsl() {
+        //member1을 찾아라.
+        JPAQueryFactory queryFactory = new JPAQueryFactory(em);
+        Member findMember = queryFactory
+                .select(member)
+                .from(member)
+                .where(member.username.eq("member1"))//파라미터 바인딩 처리
+                .fetchOne();
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
+
+    @Test
+    void resultFetch() {
+        long result = queryFactory
+                .selectFrom(member)
+                .fetchCount();
+
+        System.out.println(result);
+
+    }
+
+    @Test
+    void sort() {
+        em.persist(new Member(null, 100));
+        em.persist(new Member("member5", 100));
+        em.persist(new Member("member6", 100));
+
+        List<Member> result = queryFactory
+                .selectFrom(member)
+                .where(member.age.eq(100))
+                .orderBy(member.age.desc(), member.username.asc().nullsLast())
+                .fetch();
+
+        System.out.println(result.get(0).getUsername());
+        System.out.println(result.get(1).getUsername());
+
+    }
+
+    @Test
+    void aggregation() {
+        Tuple result = queryFactory
+                .select(
+                        member.count(),
+                        member.age.sum(),
+                        member.age.avg(),
+                        member.age.max()
+                )
+                .from(member)
+                .fetchOne();
+
+        System.out.println(result.toString());
+    }
+
+    @Test
+    void group() {
+        List<Tuple> result = queryFactory
+                .select(team.name, member.age.avg())
+                .from(member)
+                .join(member.team, team)
+                .groupBy(team.name)
+                .fetch();
+
+        System.out.println(result.toString());
+    }
+
+
+    @Test
+    void join() {
+        List<Member> result = queryFactory
+                .selectFrom(member)
+                .join(member.team, team)
+                .where(team.name.eq("teamA"))
+                .fetch();
+        System.out.println(result.get(0).toString());
+    }
+
+
+    // 연관관계가 없는 외부조인
+    @Test
+    void join_on_filtering() {
+        List<Tuple> result = queryFactory
+                .select(member, team)
+                .from(member)
+                .leftJoin(member.team, team)
+                .on(team.name.eq("teamA"))
+                .fetch();
+
+        for (Tuple tuple : result) {
+            System.out.println(tuple);
+        }
+    }
+
+    @Test
+    void join_non_filtering() {
+        em.persist(new Member("teamA"));
+
+        List<Tuple> result = queryFactory
+                .select(member, team)
+                .from(member)
+                .leftJoin(team)
+                .on(member.username.eq(team.name))
+                .fetch();
+
+        for (Tuple tuple : result) {
+            System.out.println(tuple);
+        }
+    }
+
+    @PersistenceUnit
+    EntityManagerFactory emf;
+
+
+    @Test
+    void fetchJoinNo() {
+        em.flush();
+        em.clear();
+
+        Member result = queryFactory
+                .selectFrom(member)
+                .where(member.username.eq("member1"))
+                .fetchOne();
+
+        System.out.println(result.toString());
+    }
+
+    @Test
+    void subQuery() {
+
+        QMember subMember = new QMember("sub");
+
+        List<Member> result = queryFactory
+                .selectFrom(member)
+                .where(member.age.eq(
+                        JPAExpressions
+                                .select(subMember.age.max())
+                                .from(subMember)
+
+                ))
+                .fetch();
+
+        assertThat(result).extracting("age").containsExactly(40);
+
+    }
+
+    @Test
+    void findDtoBySetter() {
+        List<MemberDto> result = queryFactory
+                .select(
+                        Projections.fields(
+                                MemberDto.class,
+                                member.username,
+                                member.age
+                        ))
+                .from(member)
+                .fetch();
+
+        for (MemberDto memberDto : result) {
+            System.out.println(memberDto.toString());
+        }
+    }
 }
